c30a616466f740bfb30d99fb39cfb2636beb637f
abhi14m <abhishekmeena1432001@gmail.com>
2023-10-19 07:15:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(()=>{console.log('red')},4000);
+
